ec5095b1ab5339e4e42740013d970e2094366fe7
Lunexdusete <166276621+Lunexdusete@users.noreply.github.com>
2024-04-13 22:55:47 -0300

    update Index.lua

--- Index.lua
+++ Index.lua
@@ -1,3 +1,2 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Lunexdusete/penisgrosso/main/Index.lua",true))()
-
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/thanhdat4461/OrionMoblie/main/source')))()
