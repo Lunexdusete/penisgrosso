90d67db2293a9fcafbb69e47e6a37c310d3bff96
Lunexdusete <166276621+Lunexdusete@users.noreply.github.com>
2024-04-13 23:07:57 -0300

    update Index.lua

--- Index.lua
+++ Index.lua
@@ -1,6 +1,11 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Lunexdusete/penisgrosso/main/Index.lua",true))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/thanhdat4461/OrionMoblie/main/source')))()
 
+local Window = OrionLib:MakeWindow({Name = "Zoombie Tyconn | Farm ",
+  HidePremium = false,
+  SaveConfig = true,
+  ConfigFolder = "@roopzinnk7"})
+
 -- VARIAVEIS
 _G.AutoResearch = false
 function AutoResearch()
@@ -9,5 +14,8 @@ function AutoResearch()
   wait(0.0001)
   end
 end
+  
+-- Menu 
+
   
 OrionLib:Init()
