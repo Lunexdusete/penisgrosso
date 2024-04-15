fa666c66eee292d1a3f51fe2ceeb9ae9eb0016a4
Lunexdusete <166276621+Lunexdusete@users.noreply.github.com>
2024-04-14 23:29:23 -0300

    update Index.lua

--- Index.lua
+++ Index.lua
@@ -1,16 +1,45 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Lunexdusete/penisgrosso/main/Index.lua",true))()
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/thanhdat4461/OrionMoblie/main/source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({
-    Name = "Lost Hub | Speed Simulator",
-    HidePremium = false,
-    IntroText = "Lost Hub",
-    SaveConfig = false,
-    ConfigFolder = "Click"
+  Name = "Lost Hub | Legends of speed",
+  HidePremium = false,
+  IntroText = "Lost Hub",
+  SaveConfig = false,
+  ConfigFolder = "Legends of speed"})
+OrionLib:MakeNotification({
+	Name = "Última atualização - 14/04/2024",
+	Content = "Este script pode estar desatualizado devido a possíveis atualizações do jogo.",
+	Image = "rbxassetid://4483345998",
+	Time = 9
 })
-local Tab = Window:MakeTab({
-	Name = "Tab 1",
+
+-- Valores
+_G.Farm = false
+function Farm()
+  while _G.Farm == true do
+      Aqui o codigo copiado do simple spy
+    wait(5)
+   end
+end
+
+-- Menu
+local Menu = Window:MakeTab({
+	Name = "Menu",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
+local Section = Menu:AddSection({
+	Name = "Auto Farm"
+})
+Menu:AddToggle({
+	Name = "Auto Farm",
+	Default = false,
+	Callback = function(Value)
+		_G.Farm = Value
+		Farm()
+	end
+})
 
-OrionLib:init()
+Menu:AddLabel("Instagram: @roopzinnk7")
+
+OrionLib:Init()
